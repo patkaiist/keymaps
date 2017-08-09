c8310c440833846da942f3510812c15d421d6222
柯禕藍 <yhilan.ko@gmail.com>
2017-08-09 13:53:13 +1000

    Update keymap.c

--- planck/keymap.c
+++ planck/keymap.c
@@ -1,21 +1,21 @@
 #include "planck.h"
 #include "action_layer.h"
 
-enum preonic_layers {
+enum planck_layers {
     _QWERTY,
     _ADJUST
 };
 
-enum preonic_keycodes {
+enum planck_keycodes {
     QWERTY = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = {
-    {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
+    {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,         KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,    KC_N,    KC_M,    KC_HOME, KC_COMM, KC_DOT,  KC_SLSH },
-    {_______, KC_LCTL, KC_LALT, KC_LGUI, MO(_ADJUST),  _______, KC_SPC,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENT}
+    {KC_BSLS, KC_LCTL, KC_LALT, KC_LGUI, MO(_ADJUST),  _______, KC_SPC,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENT}
     },
     [_ADJUST] = {
     {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,         KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
