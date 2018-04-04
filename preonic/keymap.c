7f8a396fa807d97f6ab1771d0bf08ac0faab86f0
柯禕藍 <yhilan.ko@gmail.com>
2018-04-05 09:27:17 +1000

    forgot shift

--- preonic/keymap.c
+++ preonic/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{KC_TAB,	KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_BSLS},
 		{KC_VOLU,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT},
 		{KC_VOLD,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_MINS,	KC_EQL},
-		{KC_LCTL,	KC_PSCR,	KC_LALT,	KC_LGUI,	MO(_ADJ),	KC_SPC,		KC_SPC,		KC_LBRC,	KC_RBRC,	KC_GRV,		KC_SLSH.	KC_ENT}
+		{KC_LSFT,	KC_LCTL,	KC_LALT,	KC_LGUI,	MO(_ADJ),	KC_SPC,		KC_SPC,		KC_LBRC,	KC_RBRC,	KC_GRV,		KC_SLSH,	KC_ENT}
 	},
 	[_ADJ] = {
 		{KC_F12,	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11},
@@ -25,3 +25,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{_______,	_______,	KC_TRNS,	_______,	KC_TRNS,	_______,	_______,	KC_VOLD,	KC_VOLU,	_______,	_______,	_______}
 	}
 };
+
+#ifdef AUDIO_ENABLE
+float tone_startup[][2]    = {
+	{NOTE_E6, 10}, {NOTE_E5, 10}
+};
+#endif
+
+void matrix_init_user(void) {
+	#ifdef AUDIO_ENABLE
+		startup_user();
+	#endif
+}
+
+void startup_user() {
+	_delay_ms(40); // gets rid of tick
+	PLAY_SONG(tone_startup);
+}
